3846b1f2b18e854033242e079d0942389a457e4c
Zibo LIU <785214433@qq.com>
2025-06-05 00:58:03 +0800

    Android requests microphone permissions

--- android/src/main/java/com/example/sweety8note/android/AndroidLauncher.java
+++ android/src/main/java/com/example/sweety8note/android/AndroidLauncher.java
@@ -1,18 +1,30 @@
 package com.example.sweety8note.android;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.example.sweety8note.Sweety8NoteGame;
 
-/** Launches the Android application. */
+/**
+ * Launches the Android application.
+ */
 public class AndroidLauncher extends AndroidApplication {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
-        configuration.useImmersiveMode = true; // Recommended, but not required.
-        initialize(new Sweety8NoteGame(), configuration);
+
+        // Android 6.0+ 动态请求权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+            }
+        }
+
+        initialize(new Sweety8NoteGame(), new AndroidApplicationConfiguration());
     }
+
 }
