abca25f7a10ce5b71a74109b3f2809eb1a62fd37
qsy <2074474233@qq.com>
2025-06-07 15:38:02 +0800

    feat: Modify MainMenuScreen and GameOverScreen

--- core/src/main/java/com/example/sweety8note/MainMenuScreen.java
+++ core/src/main/java/com/example/sweety8note/MainMenuScreen.java
@@ -3,101 +3,112 @@ package com.example.sweety8note;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 
 public class MainMenuScreen implements Screen {
-    final Sweety8NoteGame game;
-    private Stage stage;
-    private Texture background;
 
-    public MainMenuScreen(final Sweety8NoteGame game) {
+    private final Sweety8NoteGame game;
+    private final SpriteBatch batch;
+    private final Stage stage;
+    private final Texture background;
+
+    // ---- 纹理缓存，方便最后统一 dispose ----
+    private Texture startUpTex, startDownTex;
+    private Texture exitUpTex, exitDownTex;
+
+    public MainMenuScreen(Sweety8NoteGame game) {
         this.game = game;
-        stage = new Stage(new ScreenViewport());
+        this.batch = game.getBatch();
+        this.stage = new Stage(new ScreenViewport(), batch);
         Gdx.input.setInputProcessor(stage);
 
-        try {
-            background = new Texture("mainmenu.png");
-            System.out.println("背景图片加载成功！");
-        } catch (Exception e) {
-            System.out.println("背景图片加载失败！");
-            background = new Texture("white_pixel.png");  // 用白图代替
-        }
+        /* ---------- 背景 ---------- */
+        background = new Texture("mainmenu.png");
 
-        BitmapFont font = new BitmapFont();
-        font.getData().setScale(2f);
+        /* ---------- 按钮纹理 ---------- */
+        startUpTex = new Texture("start.png"); // 使用你的按钮素材
+        startDownTex = new Texture("start.png"); // 可以使用同一张图
+        exitUpTex = new Texture("quit.png");
+        exitDownTex = new Texture("quit.png");
 
-        TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
-        buttonStyle.font = font;
-        buttonStyle.fontColor = Color.WHITE;
+        /* ---------- 使用 ImageButton ---------- */
+        ImageButton startBtn = makeButton(startUpTex, startDownTex);
+        ImageButton exitBtn = makeButton(exitUpTex, exitDownTex);
 
-        try {
-            Texture up = new Texture("button_up.png");
-            Texture down = new Texture("button_down.png");
-            buttonStyle.up = new TextureRegionDrawable(new TextureRegion(up));
-            buttonStyle.down = new TextureRegionDrawable(new TextureRegion(down));
-        } catch (Exception e) {
-            System.out.println("按钮图像加载失败，使用默认颜色！");
-            Texture white = new Texture("white_pixel.png");
-            buttonStyle.up = new TextureRegionDrawable(new TextureRegion(white)).tint(Color.DARK_GRAY);
-            buttonStyle.down = new TextureRegionDrawable(new TextureRegion(white)).tint(Color.GRAY);
-        }
-
-        TextButton startButton = new TextButton("Start", buttonStyle);
-        TextButton exitButton = new TextButton("Exit", buttonStyle);
-
+        /* ---------- 布局 ---------- */
         Table table = new Table();
         table.setFillParent(true);
         table.center();
-        table.add(startButton).padBottom(10).row();
-        table.add(exitButton);
+        table.add(startBtn).padBottom(20).row();
+        table.add(exitBtn);
         stage.addActor(table);
 
-        startButton.addListener(new ClickListener() {
+        /* ---------- 点击逻辑 ---------- */
+        startBtn.addListener(new ClickListener() {
             @Override
-            public void clicked(InputEvent event, float x, float y) {
-                System.out.println("Start按钮点击，进入GameScreen！");
+            public void clicked(InputEvent e, float x, float y) {
                 game.setScreen(new GameScreen(game));
             }
         });
-
-        exitButton.addListener(new ClickListener() {
+        exitBtn.addListener(new ClickListener() {
             @Override
-            public void clicked(InputEvent event, float x, float y) {
-                System.out.println("退出按钮点击！");
+            public void clicked(InputEvent e, float x, float y) {
                 Gdx.app.exit();
             }
         });
     }
 
-    @Override public void show() {}
+    /* 工具：把两张纹理封装成 ImageButton */
+    private ImageButton makeButton(Texture up, Texture down) {
+        ImageButton.ImageButtonStyle s = new ImageButton.ImageButtonStyle();
+        s.up = new TextureRegionDrawable(up);
+        s.down = new TextureRegionDrawable(down);
+        return new ImageButton(s);
+    }
 
+    /* ---------- 渲染 ---------- */
     @Override
-    public void render(float delta) {
-        game.batch.begin();
-        game.batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        game.batch.end();
+    public void render(float dt) {
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
 
-        stage.act(delta);
+        stage.act(dt);
         stage.draw();
     }
 
-    @Override public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
+    /* ---------- 其他接口 ---------- */
+    @Override
+    public void resize(int w, int h) {
+        stage.getViewport().update(w, h, true);
     }
 
-    @Override public void pause() {}
-    @Override public void resume() {}
-    @Override public void hide() {}
-    @Override public void dispose() {
+    @Override
+    public void show() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void dispose() {
         stage.dispose();
         background.dispose();
+        startUpTex.dispose();
+        startDownTex.dispose();
+        exitUpTex.dispose();
+        exitDownTex.dispose();
     }
 }
